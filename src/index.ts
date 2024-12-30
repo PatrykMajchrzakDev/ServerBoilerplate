50e5f08d22a17aa5052111b11f43a70c220e8e26
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-12-30 16:13:46 +0100

    Commented for now passport so I can test api calls. Added cors config and updated env variables with app.config function

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import passport from "passport";
+import cookieParser from "cookie-parser";
+import { config } from "@/config/app.config";
 
 const app = express();
 
@@ -10,18 +12,27 @@ const app = express();
 // <!-- ====== Middleware ====== -->
 // <!-- ======================== -->
 
-//
+//Cookies
+app.use(cookieParser());
 
 // Initialize passport and session
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Automatically convert the body of any request to server as JSON
 app.use(express.json());
+
 // Cors to handle cross domain resource sharing
-app.use(cors());
+app.use(
+  cors({
+    // credentials prop handles sending cookie credentials to client
+    credentials: true,
+    origin: config.BACKEND_BASE_URL,
+  })
+);
 //Middleware that parses info sent to backend and changes it to human readable format
 app.use(express.urlencoded({ extended: true }));
+
 // <!-- ======================== -->
 // <!-- ======== ROUTING ======= -->
 // <!-- ======================== -->
@@ -36,6 +47,6 @@ app.get("/test", async (req: Request, res: Response) => {
 // <!-- ======================== -->
 // <!-- ===== SERVER START ===== -->
 // <!-- ======================== -->
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server started on localhost:${process.env.PORT}`);
+app.listen(config.PORT || 4000, () => {
+  console.log(`Server started port:${config.PORT} in ${config.NODE_ENV}`);
 });
