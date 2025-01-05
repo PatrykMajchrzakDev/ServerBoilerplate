0801034c3460c149eaf77be77ccf27871f4ffc6a
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-01-05 15:24:06 +0100

    Added login controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,8 +4,12 @@ import { asyncHandler } from "@/middleware/asyncHandler";
 import { AuthService } from "./auth.service";
 import { Request, Response } from "express";
 import { HTTPSTATUS } from "@/config/http.config";
-import { registerSchema } from "@/common/validation/auth.validator";
+import {
+  loginSchema,
+  registerSchema,
+} from "@/common/validation/auth.validator";
 import { stripUserToFrontend } from "@/utils/destructureResponse";
+import { setAuthenticationCookies } from "@/utils/cookie";
 
 // Parent class invoking auth
 export class AuthController {
@@ -17,6 +21,8 @@ export class AuthController {
 
   // Validates JSON, runs REGISTER method from ./auth.service.ts
   // and returns message with object if successfull.
+
+  // ============== REGISTER ===============
   public register = asyncHandler(
     async (req: Request, res: Response): Promise<any> => {
       const body = registerSchema.parse({
@@ -27,6 +33,30 @@ export class AuthController {
         message: "User registered successfully.",
         data: stripUserToFrontend(user),
       });
+    }
+  );
+
+  // ================ LOGIN ================
+  public login = asyncHandler(
+    async (req: Request, res: Response): Promise<any> => {
+      const userAgent = req.headers["user-agent"];
+      const body = loginSchema.parse({
+        ...req.body,
+        userAgent,
+      });
+
+      // Destructure object from auth.service.ts
+      const { user, accessToken, refreshToken, mfaRequired } =
+        await this.authService.login(body);
+
+      // Set cookies
+      return setAuthenticationCookies({ res, accessToken, refreshToken })
+        .status(HTTPSTATUS.OK)
+        .json({
+          message: "User logged in successfully.",
+          user: stripUserToFrontend(user),
+          mfaRequired,
+        });
     }
   );
 }
