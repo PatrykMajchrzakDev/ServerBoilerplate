dd9e81611778c3fd3aa98a63d3c84d83c2a0f3cf
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-01-09 16:25:55 +0100

    Added route for forgot password api

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -9,5 +9,6 @@ authRoutes.post("/register", authController.register);
 authRoutes.post("/login", authController.login);
 authRoutes.get("/refresh", authController.refreshToken);
 authRoutes.post("/verify/email", authController.verifyEmail);
+authRoutes.post("/password/forgot", authController.forgotPassword);
 
 export default authRoutes;
