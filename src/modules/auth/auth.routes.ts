a7383e36e8f1a570026282e87cbcac27a8124c35
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-01-23 17:13:33 +0100

    Added route for resending email verification code

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -9,6 +9,7 @@ const authRoutes = Router();
 authRoutes.post("/register", authController.register);
 authRoutes.post("/login", authController.login);
 authRoutes.post("/verify/email", authController.verifyEmail);
+authRoutes.post("/resend/email-verification", authController.resendEmailVerification);
 authRoutes.post("/password/forgot", authController.forgotPassword);
 authRoutes.post("/password/reset", authController.resetPassword);
 authRoutes.post("/logout", authenticateJWT, authController.logout);
