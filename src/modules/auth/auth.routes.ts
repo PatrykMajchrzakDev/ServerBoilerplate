e640b6a434bf928a39a58a55a43a16c866a1961d
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-01-30 18:47:02 +0100

    Updated routing with google auth

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -3,17 +3,44 @@
 import { Router } from "express";
 import { authController } from "./auth.module";
 import { authenticateJWT } from "@/common/strategies/jwt.strategy";
+import { config } from "@/config/app.config";
+import passport from "passport";
 
 const authRoutes = Router();
 
+const failedUrl = `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`;
+
+// ================= LOCAL ROUTES ==================
 authRoutes.post("/register", authController.register);
 authRoutes.post("/login", authController.login);
 authRoutes.post("/verify/email", authController.verifyEmail);
-authRoutes.post("/resend/email-verification", authController.resendEmailVerification);
+authRoutes.post(
+  "/resend/email-verification",
+  authController.resendEmailVerification
+);
 authRoutes.post("/password/forgot", authController.forgotPassword);
 authRoutes.post("/password/reset", authController.resetPassword);
 authRoutes.post("/logout", authenticateJWT, authController.logout);
 
 authRoutes.get("/refresh", authController.refreshToken);
+
+// ================= GOOGLE ROUTES ==================
+
+// Login via Google
+authRoutes.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// Google redirect to me
+authRoutes.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: failedUrl,
+  }),
+  authController.googleLoginCallback
+);
 
 export default authRoutes;
