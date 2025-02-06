553bdb04793747481a1478723bd53ae28d02551c
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-02-06 18:50:37 +0100

    Updated the way i validate passwords and names to match frontend as well

--- src/common/validation/auth.validator.ts
+++ src/common/validation/auth.validator.ts
@@ -3,7 +3,29 @@
 import { z } from "zod";
 
 export const emailSchema = z.string().trim().email().min(1).max(255);
-export const passwordSchema = z.string().trim().min(6).max(255);
+
+// Name schema
+export const userNameSchema = z
+  .string()
+  .trim()
+  .min(3, "Minimum length of 3")
+  .max(30, "Maximum length of 30");
+
+// Password should be min 8, have lowercase, uppercase letters and numbers
+export const passwordSchema = z
+  .string()
+  .trim()
+  .min(8, "Password should have at least 8 characters")
+  .max(60, "Password should not be longer than 60 characters")
+  .refine(
+    (val) => /[a-z]/.test(val ?? ""),
+    "Password should include lowercase letters"
+  )
+  .refine(
+    (val) => /[A-Z]/.test(val ?? ""),
+    "Password should include uppercase letters"
+  )
+  .refine((val) => /[0-9]/.test(val ?? ""), "Password should include numbers");
 export const verificationCodeSchema = z.string().trim().min(1).max(25);
 
 // ==================================================
@@ -13,7 +35,7 @@ export const verificationCodeSchema = z.string().trim().min(1).max(25);
 // Register schema
 export const registerSchema = z
   .object({
-    name: z.string().trim().min(1).max(255),
+    name: userNameSchema,
     email: emailSchema,
     password: passwordSchema,
     confirmPassword: passwordSchema,
